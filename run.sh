d0b534a8e372f2ff73e8e3f817d827de423d07a6
Pablo Ferrero <ferreronicolas58@gmail.com>
2019-11-21 15:03:37 -0300

    Script Corrijo el script run para que compile a 32 bits.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-gcc main.c enmascarar_c.c -o ejec
+#nasm -f elf32 enmascarar_asm.s -o enmascarar_asm.o
+#gcc -m32 main.c enmascarar_c.c enmascarar_asm.o -o ejec
+gcc -m32 main.c enmascarar_c.c -o ejec
 ./ejec img/desierto800x600.rgb img/peces800x600.rgb img/mascara800x600.rgb 800 600
 ./ejec img/cielo1920x1080.rgb img/tiburon1920x1080.rgb img/mascara1920x1080.rgb 1920 1080
 ./ejec img/atardecer3840x2160.rgb img/iceberg3840x2160.rgb img/mascara3840x2160.rgb 3840 2160
