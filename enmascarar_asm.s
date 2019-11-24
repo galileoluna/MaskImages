b3e30c1df881b0ff601767f8bcd19ff0b698179c
Galileo Luna <galileoungs@gmail.com>
2019-11-24 15:30:01 +0000

    limpio registros e iniciabilizo los registros-

--- enmascarar_asm.s
+++ enmascarar_asm.s
@@ -5,6 +5,28 @@ section .data
 section .text
     global _enmascarar_asm  
 _enmascarar_asm:
+    push ebp
+    push eax
+    push ebx
+    push ecx
+    push edx    
+    mov ebp, esp 
+    mov eax, 0
+    mov ebx, 0
+    mov ecx, 0
+    mov edx, 0
+    xor edx, edx
+    mov eax, dword[ebp+20]
+    add ebx, 8
+    div ebx
+    mov ecx, eax
+    _mmxLoop:
+    _mmxMask:
+    _Endmmx:
+
+     ret
+
+
 
 
 ret
