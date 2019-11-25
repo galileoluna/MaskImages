2fba070669aa339363623acf0d27910b170ff25a
Galileo Luna <galileoungs@gmail.com>
2019-11-25 04:48:38 +0000

    Update enmascarar_asm.s

--- enmascarar_asm.s
+++ enmascarar_asm.s
@@ -3,7 +3,8 @@
 section .data
 
 section .text
-    global _enmascarar_asm  
+    global _enmascarar_asm    
+
 _enmascarar_asm:
     push ebp
     push eax
@@ -20,15 +21,31 @@ _enmascarar_asm:
     add ebx, 8
     div ebx
     mov ecx, eax
-    _mmxLoop:
+   _mmxLoop:
+        mov eax, ecx
+        mov ebx, 8
+        mul ebx
+        sub eax, 8
+        mov ebx,[ebp+8]
+        add ebx, eax
+        movd mm0 ,[ebx]
+        mov ebx, [ebp+12]
+        add ebx, eax
+        movd mm1,[ebx]
     _mmxMask:
+        mov ebx, [ebp+16]
+        add ebx, eax
+        movd mm2,[ebx]
+
+        pand mm1, mm2   
+        pand mm2, mm0  
+        por mm1, mm2    
+        mov ebx, [ebp+8] ; img1
+        add ebx, eax
+        movd [ebx], mm1
+        loop mmxLoop
     _Endmmx:
-
-     ret
-
-
-
-
-ret
-
-
+    emms 
+    mov esp, ebp 
+    pop ebp 
+    ret
