212eb1fb274039d2a5e33db769a4a3468242cb07
Pablo Ferrero <ferreronicolas58@gmail.com>
2019-11-26 12:35:52 -0300

    Script Mejoro el script para limpiar el directorio y agrego un for al script run para redimensionar la primera imagen y ejecutar el programa más veces.

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 rm ejec
-#rm enmascarar_asm.o
-#rm salida_asm800x600.rgb
-#rm salida_asm1920x1080.rgb
-#rm salida_asm3840x2160.rgb
-rm salida_c800x600.rgb
-rm salida_c1920x1080.rgb
-rm salida_c3840x2160.rgb
+rm enmascarar_asm.o
+rm *.rgb
